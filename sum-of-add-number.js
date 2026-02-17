37b7dcae0092521d1d21be75a7812035313ed854
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2026-02-17 10:33:59 +0600

    sum of add number

---
+++ sum-of-add-number.js
@@ -0,0 +1,15 @@
+let sum1 = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 !== 0) {
+    sum1 = sum1 + i;
+  }
+}
+console.log(sum1);
+
+let sum2 = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    sum2 = sum2 + i;
+  }
+}
+console.log(sum2);
